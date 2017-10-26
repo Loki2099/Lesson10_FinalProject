dd2e1c29ac1a80ad5cda047c927f028162b7531d
Loki2099 <33090971+Loki2099@users.noreply.github.com>
2017-10-25 20:14:51 -0500

    adjusted jscrip
    cleaned up result display and added word count at punctuation.

--- script.js
+++ script.js
@@ -17,18 +17,14 @@
 
         if (current === "." || current === "!" || current === "?"){
             lineCount++;
-            wordCount++;
+            if(i === inputVal.length -1) wordCount++;
         }
     }
 
     avgWords = wordCount / lineCount;
 
-    var r1 = document.getElementById("r1");
-    r1.innerHTML = "There are an average of " + avgWords.toString() + " words per sentence.";
-    var r2 = document.getElementById("r2");
-    r2.innerHTML = "There are " + spaceCount.toString() + " spaces in this text.";
-    var r3 = document.getElementById("r3");
-    r3.innerHTML = "There are " + wordCount.toString() + " words in this text.";
-    var r4 = document.getElementById("r4");
-    r4.innerHTML = "There are " + lineCount.toString() + " sentences in this text.";
+    document.getElementById("r1").innerHTML = "There are an average of " + avgWords.toString() + " words per sentence.";
+    document.getElementById("r2").innerHTML = "There are " + spaceCount.toString() + " spaces in this text.";
+    document.getElementById("r3").innerHTML = "There are " + wordCount.toString() + " words in this text.";
+    document.getElementById("r4").innerHTML = "There are " + lineCount.toString() + " sentences in this text.";
 }
